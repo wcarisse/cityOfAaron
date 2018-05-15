cffc5849890e44fd08182aaf2550dd72dd37c9af
CarissaC <Cun11003@byui.edu>
2018-05-14 20:44:36 -0700

    05-14-2018 Created cityOfAaron project!!! Woot!

---
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Spring 2018
+ * Team Members: Carissa Cunningham, Kate Lewis, Kyli Barnes
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author Joshua
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
