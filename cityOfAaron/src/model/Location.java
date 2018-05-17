afc73a48ed6b76566fffdf573b6e63a2751219cc
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-05-16 23:18:46 -0400

    5-16-18 Kate Location class and location main method() testing

---
+++ cityOfAaron/src/model/Location.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author kate
+ */
+public class Location implements Serializable{
+    //class instance variables
+    private String description;
+    private String symbol;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Location() {
+    }
+    
+    
+}
