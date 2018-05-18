f7b7dd81c67131cfb2247556f730ae3b3f8eaa10
kayjb <38710045+kayjb@users.noreply.github.com>
2018-05-18 10:43:31 -0600

    5/18/18 - kb created the class ListItem and ran test code in the main () folder

---
+++ cityOfAaron/src/model/ListItem.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kyli Barnes
+ */
+public class ListItem implements Serializable{
+    private String name;
+    private int number;
+
+    public ListItem() {
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    
+    
+}
